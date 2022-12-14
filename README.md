316f59c0d306837bfb0568e9b068d2b3b5f32090
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2022-12-14 19:43:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso_Java_Cod3r
+ Execicios de Java do curso da Cod3r
---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package fundamentos;
+/**
+ * @date 12/12/2022
+ * @author pedro
+ *
+ */
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
