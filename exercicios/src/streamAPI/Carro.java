8ebb737aa258cb479d7f9a839d9e68c30c5c72ac
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-05-22 22:11:49 -0300

    Aulas do módulo de StreamAPI

---
+++ exercicios/src/streamAPI/Carro.java
@@ -0,0 +1,16 @@
+package streamAPI;
+
+public class Carro {
+
+	final String modelo;
+	final double preco;
+	final boolean economico;
+	
+	public Carro(String modelo, double preco, boolean economico) {
+		super();
+		this.modelo = modelo;
+		this.preco = preco;
+		this.economico = economico;
+	}
+		
+}
