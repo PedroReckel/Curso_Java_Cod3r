09347037f63e78f4ce350274b87ada954d1796db
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-06-06 23:38:14 -0300

    Módulo de Stream API

---
+++ exercicios/src/streamAPI/Reduce2.java
@@ -0,0 +1,33 @@
+package streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Reduce2 {
+
+	public static void main(String[] args) {
+		
+		Aluno a1 = new Aluno("Joao", 10.0);
+		Aluno a2 = new Aluno("Pedro", 8.0);
+		Aluno a3 = new Aluno("Kaua", 6.8);
+		Aluno a4 = new Aluno("Vitoia", 4.5);
+		Aluno a5 = new Aluno("Jorge", 7.4);
+		
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5);
+	
+		Predicate<Aluno> aprovado = a -> a.nota >= 7;
+		Function<Aluno, Double> apenasNota = a -> a.nota;
+		BinaryOperator<Double> somatorio = (a, b) -> a + b;
+		
+		alunos.parallelStream()
+			.filter(aprovado)
+			.map(apenasNota)
+			.reduce(somatorio)
+			.ifPresent(System.out::println);
+		
+	}
+	
+}
