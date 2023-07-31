d3ab7781e8541e47abd638aaa478fa7e8cee9126
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-07-31 15:45:02 -0300

    Módulo Projeto Campo Minado

--- Campo-minado/test/br/com/epsilon/cm/modelo/CampoTeste.java
+++ Campo-minado/test/br/com/epsilon/cm/modelo/CampoTeste.java
@@ -1,5 +1,7 @@
 package br.com.epsilon.cm.modelo;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.concurrent.locks.Condition;
@@ -135,5 +137,105 @@ public class CampoTeste {
 		
 		assertTrue(campo22.isAberto() && campo11.isFechado());
 	}
+	
+	@Test
+	void testeGetColuna() {
+		Campo c1 = new Campo(1, 4);
+		
+		int colunaEsperada = 4;
+		int colunaRetornada = c1.getColuna();
+		
+		assertEquals(colunaEsperada, colunaRetornada);
+	}
+
+	@Test
+	void testeGetLinha() {
+		Campo c1 = new Campo(1, 4);
+		
+		int linhaEsperada = 1;
+		int linhaRetornada = c1.getLinha();
+		
+		assertEquals(linhaEsperada, linhaRetornada);
+	}
+	
+	@Test
+	void testeobjetivoAlcancado() {
+		Campo c1 = new Campo(2, 4);
+		c1.abrir();
+		Campo c2 = new Campo(3, 3);
+		c2.minar();
+		c2.alternarMarcacao();
+		
+		assertTrue(c1.objetivoAlcancado() || c2.objetivoAlcancado());		
+		
+	}
+	
+	@Test
+	void testeReiniciar() {
+		Campo c1 = new Campo(3, 2);
+		c1.abrir();
+		c1.minar();
+		c1.alternarMarcacao();
+		
+		c1.reiniciar();
+		
+		assertFalse(c1.isAberto());
+		assertFalse(c1.isMinado());
+		assertFalse(c1.isMarcado());
+	}
+	
+	
+	@Test
+    void testeToStringCampoMarcado() {
+    	Campo campo = new Campo(1, 1);
+    	campo.alternarMarcacao();
+    	
+    	String retornoEsperado = "x";
+    	String retorno = campo.toString();
+    	
+    	assertEquals(retornoEsperado, retorno);
+    }
+    
+    @Test
+    void testeToStringCampoAbertoComMina() {
+        Campo campo = new Campo(2, 2);
+        campo.abrir();
+        campo.minar();
+        
+        String retornoEsperado = "*";
+        String retorno = campo.toString();
+        assertEquals(retornoEsperado, retorno);
+    }
+    
+    @Test
+    void testeToStringCampoAbertoComVizinhacaMinada() {
+    	Campo campo = new Campo(3, 3);
+    	campo.abrir();
+    	campo.adicionarVizinho(new Campo(2, 3));
+    	campo.getVizinhos().forEach(v -> v.minar());
+    	
+    	String retornoEsperado = "1";
+    	String retorno = campo.toString();
+    	assertEquals(retornoEsperado, retorno);
+    }
+    
+    @Test
+    void testeToStringCampoAbertoSemMinaNemVizinhacaMinada() {
+    	Campo campo = new Campo(4, 4);
+    	campo.abrir();
+    	
+    	String retornoEsperado = " ";
+    	String retorno = campo.toString();
+    	assertEquals(retornoEsperado, retorno);
+    }
+   
+    @Test
+    void testeToStringCampoFechado() {
+        Campo campo = new Campo(5, 5);
+        
+        String retornoEsperado = "?";
+        String retorno = campo.toString();
+        assertEquals(retornoEsperado, retorno);
+    }
 	
 }
