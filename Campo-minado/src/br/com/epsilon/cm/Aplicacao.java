e561402010082257d151aa46fabe33c12a71c297
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-08-27 16:50:50 -0300

    Projeto Campo Minado

--- Campo-minado/src/br/com/epsilon/cm/Aplicacao.java
+++ Campo-minado/src/br/com/epsilon/cm/Aplicacao.java
@@ -1,19 +1,14 @@
 package br.com.epsilon.cm;
 
 import br.com.epsilon.cm.modelo.Tabuleiro;
+import br.com.epsilon.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
-		
+		new TabuleiroConsole(tabuleiro);		
 	}
 	
 }
