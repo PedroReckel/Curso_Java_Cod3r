2e9609f7adef200075c251bb86e6d76aa5bc878c
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-09-26 23:08:34 -0300

    Módulo de Java Generics

--- exercicios/src/generics/Pares.java
+++ exercicios/src/generics/Pares.java
@@ -1,11 +1,14 @@
 package generics;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Pares<C extends Number, V> {
 
-	private final SortedSet<Par<C, V>> itens = new TreeSet<>();
+	private final Set<Par<C, V>> itens = new HashSet<>();
 	
 	public void adicionar(C chave, V valor) {
 		if(chave == null) return;
@@ -20,7 +23,13 @@ public class Pares<C extends Number, V> {
 	}
 	
 	public V getValor(C chave) {
-		return itens.stream().filter(null)
+		if(chave == null) return null;
+		
+		Optional<Par<C, V>> parOpcional = itens.stream()
+				.filter(par -> chave.equals(par.getChave())) // Se a chave que eu passei for igual a chave de um dos pares ele vai filtrar apenas os pares que tem a mesma chave
+				.findFirst();
+		
+		return parOpcional.isPresent() ? parOpcional.get().getValor() : null;
 	}
 	
 }
