4985565a8d3aceea0c69aec46feaa76b8ff2b710
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-12-01 22:03:46 -0300

    Projeto Calculadora usando Swing

--- calculadora/src/br/com/epsilon/calc/modelo/Memoria.java
+++ calculadora/src/br/com/epsilon/calc/modelo/Memoria.java
@@ -7,12 +7,19 @@ import java.util.List;
 // Controla-se uma classe para que ela tenha apenas uma unica instancia colocando o construtor dessa classe como privado
 
 public class Memoria {
+	
+	private enum TipoComando {
+		ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, MUDASINA;
+	};
 
 	private static final Memoria instancia = new Memoria();
 	
 	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	
+	private TipoComando ultimaOperacao = null;
+	private Boolean substituir = false;
 	private String textoAtual = "";
+	private String textoBuffer = "";
 	
 	private Memoria() {
 		
@@ -30,14 +37,66 @@ public class Memoria {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
 
-	public void processarComando(String valor) {
-		if("AC".equals(valor)) {
+	public void processarComando(String texto) {
+		
+		TipoComando tipoComando = detectarTipoComando(texto);
+		System.out.println(tipoComando);
+		
+		if(tipoComando == null) {
+			return;
+		} else if(tipoComando == TipoComando.ZERAR) {
 			textoAtual = "";
+			textoBuffer = "";
+			substituir = false;
+			ultimaOperacao = null;
+		} else if(tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			textoAtual = substituir ? texto : textoAtual + texto;
+			substituir = false;
 		} else {
-			textoAtual += valor;
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
 		}
 	
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultadoOperacao() {
+		return textoAtual;
+	}
+
+	private TipoComando detectarTipoComando(String texto) {
+		
+		if(textoAtual.isEmpty() && texto == "0") {
+			return null;
+		}
+		
+		try {
+			Integer.parseInt(texto); // Validar se o texto passado é um valor numerido
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
+			// Quando não for numerico
+			if("AC".equals(texto)) {
+				return TipoComando.ZERAR;
+			} else if("/".equals(texto)) {
+				return TipoComando.DIV;
+			} else if("*".equals(texto)) {
+				return TipoComando.MULT;
+			} else if("+".equals(texto)) {
+				return TipoComando.SOMA;
+			} else if("-".equals(texto)) {
+				return TipoComando.SUB;
+			} else if("=".equals(texto)) {
+				return TipoComando.IGUAL;
+			} else if(",".equals(texto) && !textoAtual.contains(",")) {
+				return TipoComando.VIRGULA;
+			} else if("+/-".equals(texto)) {
+				return TipoComando.MUDASINA;
+			}
+		}
+
+		return null;
 	}
 	
 }
