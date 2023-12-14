1c621a5bca35dcd98dcf639747cc4af60cb1a038
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-12-13 23:17:33 -0300

    Módulo de banco de dados e JDBC

--- jdbc/src/jdbc/FabricaConexao.java
+++ jdbc/src/jdbc/FabricaConexao.java
@@ -1,21 +1,39 @@
 package jdbc;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:postgresql://localhost:5432/curso_java";
-			final String usuario = "postgres";
-			final String senha = "";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
 			
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String diretorioHome = System.getProperty("user.home");
+		String caminhoArquivo = diretorioHome + File.separator + "conexao.properties";
+		File arquivo = new File(caminhoArquivo);
+		
+	    try (FileInputStream input = new FileInputStream(arquivo)) {
+	        prop.load(input);
+	    }
+
+	    return prop;
 	}
 	
 }
