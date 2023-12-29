f726640a559a2a1d9c347ae71e22bca05ee2be8b
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-12-28 22:27:32 -0300

    Módulo de JPA

---
+++ exercicios-jpa/src/main/java/modelo/composicao/Endereco.java
@@ -0,0 +1,31 @@
+package modelo.composicao;
+
+import javax.persistence.Embeddable;
+
+// Uma vez que eu marco uma classe como Embeddable ela não é mapeada para dentro do banco de dados
+// Esses atributos da classe Endereco serão compartilhados por varias tabelas
+@Embeddable		
+public class Endereco {
+
+	private String logradouro;
+	private String complemento;
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public String getComplemento() {
+		return complemento;
+	}
+	
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	// TODO: Persistir alguns dados nas tabelas de Fornecedor e Funcionário
+	
+}
