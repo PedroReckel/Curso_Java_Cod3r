ae82aa9d7681c5b1e5324585046f1fc8f552f206
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2024-01-27 17:05:40 -0300

    Módulo de JavaFX

--- exercicios-javafx/src/basico/Contador.java
+++ exercicios-javafx/src/basico/Contador.java
@@ -13,6 +13,19 @@ public class Contador extends Application {
 	
 	private int contador = 0;
 
+	private void atualizarLabelNumero(Label label) {
+		label.setText(Integer.toString(contador));
+		
+		label.getStyleClass().remove("verde");
+		label.getStyleClass().remove("vermelha");
+		
+		if(contador > 0) {
+			label.getStyleClass().add("verde");
+		} else if(contador < 0) {
+			label.getStyleClass().add("vermelha");
+		}
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Label labelTitulo = new Label("Contador");
@@ -21,15 +34,17 @@ public class Contador extends Application {
 		labelNumero.getStyleClass().add("numero");
 		
 		Button botaoDecremento = new Button("-");
+		botaoDecremento.getStyleClass().add("botoes");
 		botaoDecremento.setOnAction(e -> {
 			contador--;
-			labelNumero.setText(Integer.toString(contador));
+			atualizarLabelNumero(labelNumero);
 		});
 		
 		Button botaoIncremento = new Button("+");
+		botaoIncremento.getStyleClass().add("botoes");
 		botaoIncremento.setOnAction(e -> {
 			contador++;
-			labelNumero.setText(Integer.toString(contador));
+			atualizarLabelNumero(labelNumero);
 		});
 		
 		HBox boxBotoes = new HBox();
