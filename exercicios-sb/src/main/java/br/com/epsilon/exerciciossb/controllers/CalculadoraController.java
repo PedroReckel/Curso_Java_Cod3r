841ee93aba6bbdc7569735255c47779895163d6c
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2024-02-12 15:58:07 -0300

    Módulo de Spring Boot

---
+++ exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/CalculadoraController.java
@@ -0,0 +1,8 @@
+package br.com.epsilon.exerciciossb.controllers;
+
+public class CalculadoraController {
+
+//	/calculadora/somar/10/20
+//	/calculadora/subtrair?a=100&b=39
+	
+}
--- exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.epsilon.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.epsilon.exerciciossb.models.Cliente;
@@ -13,6 +15,16 @@ public class ClienteController {
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(1, "Pedro", "123.456.789-10");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente ObterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "109.876.543-21");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-44");
 	}
 	
 }
