bf794a4abce80573dd878f57bdfc2e5024207b68
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2024-02-13 15:50:22 -0300

    Módulo de Spring Boot

--- exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/CalculadoraController.java
+++ exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/CalculadoraController.java
@@ -1,8 +1,26 @@
 package br.com.epsilon.exerciciossb.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+///calculadora/somar/10/20
+///calculadora/subtrair?a=100&b=39
+
+@RestController
+@RequestMapping(path = "/calculadora")
 public class CalculadoraController {
 
-//	/calculadora/somar/10/20
-//	/calculadora/subtrair?a=100&b=39
-	
+	@GetMapping("/somar/{a}/{b}")
+	public int somar(@PathVariable int a, @PathVariable int b) {
+		return a + b;
+	}
+
+	@GetMapping(path = "/subtrair")
+	public int subtrair(@RequestParam(name = "a") int a,@RequestParam(name = "b") int b) {
+		return a - b;
+	}
+
 }
