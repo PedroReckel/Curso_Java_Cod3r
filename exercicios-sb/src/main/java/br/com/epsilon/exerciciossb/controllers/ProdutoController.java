9602d797517ed853cc3e69f779a8896ad868a496
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2024-02-17 16:38:01 -0300

    Módulo de Spring Boot

--- exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/epsilon/exerciciossb/controllers/ProdutoController.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,18 +24,38 @@ public class ProdutoController {
 	@Autowired // Com essa notação ele vai automaticamente colocar um objeto dentro desse atributo
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) { // Aqui eu uso o Valid para garantir que ele vai está aderente as regras definidas nas anotations da classe Produto
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) { // Aqui eu uso o Valid para garantir que ele vai está aderente as regras definidas nas anotations da classe Produto
 		produtoRepository.save(produto);
 		return produto;
 	}
+	
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
 	}
+	@GetMapping(path="/nome/{parteNome}")
+	public Iterable<Produto>  obterProdutosPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+		if(qtdePagina >= 5) qtdePagina = 5;
+		PageRequest page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
+	}
+	
 	@GetMapping(path = "/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
 	}
 	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+	
+	// ToDo: Fazer um softdelete
+		
 }
